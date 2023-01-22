8d8417aaa76bc8f45daef8f925695bacca02daf0
spaunder76 <arthurbousquet36@gmail.com>
2023-01-22 21:21:38 +0100

    envoi test

---
+++ cours_while.py
@@ -0,0 +1,12 @@
+A = int(input("veuillez entrer un nombre superieur a 1 "))
+
+while A<= 1:
+    A = int(input("veuillez entrer un nombre superieur a 1 "))
+S = 0
+i = 1
+
+while i<=A:
+     S = S + i
+     i = i +1
+print("la somme est : ",S)
+10
